24589f91528a90d7255ef5c998afe988c75b639d
Elijah <elijahrobb4@gmail.com>
2026-06-08 00:21:58 +0800

    Fixed middleware.ts to proxy.ts

---
+++ auth.ts
@@ -0,0 +1,19 @@
+// auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) token.id = user.id
+      return token
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
+  },
+})
